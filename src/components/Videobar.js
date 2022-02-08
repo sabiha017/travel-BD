3967dca2081622bb5fffb0113513eddcad5589cc
Fatema Sabiha <fatemasabiha553@gmail.com>
2022-02-08 12:09:40 +0100

    fixed

--- src/components/Videobar.js
+++ src/components/Videobar.js
@@ -4,7 +4,7 @@ import "./Videobar.css"
 function Videobar() {
     return (
         <div className= "video-container">
-           <video src='images/video_sunset.mp4' autoPlay loop muted />
+           <video src='/video_sunset.mp4' autoPlay loop muted />
            <h1>Visit Bangladesh</h1> 
            <p>The land of beauty</p>
         </div>
